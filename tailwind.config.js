1e7af6c19aa958cc506120340bc161bbf7fc331d
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2024-05-30 12:10:21 +0100

    First commit for the tailor application

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"], // or your preferred themes
+  },
+};
